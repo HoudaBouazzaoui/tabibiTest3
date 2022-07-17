a3bec9c6adde478cce65b1aff4f199db566d9707
boukhalid <kbouazzaoui6@gmail.com>
2022-07-17 15:20:25 +0200

    commuite de la derniere version 3 serveurs

--- praticienspecialite.sql
+++ praticienspecialite.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1
--- Généré le : lun. 31 jan. 2022 à 16:25
+-- Généré le : dim. 17 juil. 2022 à 15:17
 -- Version du serveur : 10.4.22-MariaDB
 -- Version de PHP : 8.1.1
 
