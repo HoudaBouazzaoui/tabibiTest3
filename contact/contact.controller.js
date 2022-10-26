a47b3ba1f5596d5b52a8959eee1bc825f47ecfbf
boukhalid <kbouazzaoui6@gmail.com>
2022-10-26 14:51:15 +0200

    test de commit car just avant y un pb de commit

--- contact/contact.controller.js
+++ contact/contact.controller.js
@@ -43,7 +43,6 @@ function envoyerMessage(req, res, next) {
      });
    */ 
 
-
      var transporter = nodemailer.createTransport({
         host: process.env.MAIL_HOST,
         port: process.env.MAIL_PORT,
