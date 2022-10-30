16628523ecf9453bfc6de8a3d75674de1021fbdd
boukhalid <git@github.com>
2022-10-30 13:24:52 +0100

    tast fetch

--- db/dbMysql2.js
+++ db/dbMysql2.js
@@ -20,7 +20,8 @@ async function initialize() {
     await connection.query(`CREATE DATABASE IF NOT EXISTS \`${database}\`;`);
 
     // connect to db
-    const sequelize = new Sequelize(database, user, password, { dialect: 'mysql' });
+    //const sequelize = new Sequelize(database, user, password, { dialect: 'mysql' });
+    const sequelize = new Sequelize(database, user, password, { host: host, dialect: 'mysql' });
 
     // init models and add them to the exported db object
     db.User = require('../utilisa/users/user.model')(sequelize);
